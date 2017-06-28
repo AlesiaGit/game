72640d670023baf1ad0ec2bf932b2910682a55be
AlesiaGit <stolepchenko@gmail.com>
2017-06-28 20:49:04 +0300

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var myGamePiece;
 var myEnemies = [];
+var myOtherEnemies = [];
+var myScore;
 
 function startGame() {
-	myGamePiece = new component(randomPositionX(), randomPositionY(), 20, 'red', 0, 0);
+	myGamePiece = new component(randomPositionX(), randomPositionY(), 15, 'sprite-test.png', 0, 0, 'image');
+	myScore = new component(400, 50, 'italic small-caps bold 12px arial', 'black', 0, 0, 'text');
 	myGameArea.start();
 }
 
@@ -13,6 +16,7 @@ var myGameArea = {
 		this.canvas.height = 500;
 		this.context = this.canvas.getContext('2d');
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.canvas.focus();
 		this.frameNumber = 0;
 		this.interval = setInterval(updateGameArea, 20);
 		window.addEventListener('keydown', moveGamePiece);
@@ -25,48 +29,131 @@ var myGameArea = {
 	}
 };
 
-function component(x, y, radius, color, speedX, speedY) {
+function component(x, y, radius, color, speedX, speedY, type) {
 	this.x = x;
 	this.y = y;
 	this.speedX = speedX;
 	this.speedY = speedY;
 	this.radius = radius;
-	this.update = function() {
-		ctx = myGameArea.context;
-		ctx.fillStyle = color;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-		ctx.fill();
-		ctx.closePath();
+	this.color = color;
+	this.type = type;
+	this.step = 0;
+	
+	if (type == 'image') {
+		this.image = new Image();
+		this.image.src = color;
 	}
+	
+	this.draw = function(ctx) {
+		if (this.type == 'text') {
+			ctx.font = this.radius;
+			ctx.fillStyle = this.color;
+			ctx.fillText(this.text, this.x, this.y);
+		} 
+
+		else if (this.type == 'image') {
+			this.step += 1;
+			this.step = this.step % 30;
+			var dir = 0;
+
+			if (this.speedX <= 0 && this.speedY <= 0) {
+				if (Math.abs(this.speedX) < Math.abs(this.speedY)) {
+					dir = 3;
+				} else {
+					dir = 1;
+				}
+			}
+
+			if (this.speedX <= 0 && this.speedY >= 0) {
+				if (Math.abs(this.speedX) < Math.abs(this.speedY)) {
+					dir = 0;
+				} else {
+					dir = 1;
+				}
+			}
+
+			if (this.speedX >= 0 && this.speedY <= 0) {
+				if (Math.abs(this.speedX) < Math.abs(this.speedY)) {
+					dir = 3;
+				} else {
+					dir = 2;
+				}
+			}
+
+			if (this.speedX >= 0 && this.speedY >= 0) {
+				if (Math.abs(this.speedX) < Math.abs(this.speedY)) {
+					dir = 0;
+				} else {
+					dir = 2;
+				}
+			}
+
+			if (this.speedX == 0 && this.speedY == 0) {
+				dir = 0;
+			}
+
+			ctx.drawImage(
+				this.image,												// img	Source image
+				this.image.width / 3 * Math.floor(this.step / 10),		// sx	Source x
+				this.image.height / 4 * dir,							// sy	Source y
+				this.image.width / 3,									// sw	Source width
+				this.image.height / 4,	// sh	Source height
+				this.x - this.radius,	// dx	Destination x
+				this.y - this.radius,	// dy	Destination y
+				this.radius * 2,		// dw	Destination width
+				this.radius * 2			// dh	Destination height
+			);
+		} 
+
+		else {
+			ctx.fillStyle = this.color;
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+	
 	this.newPos = function() {
 		this.x += this.speedX;
 		this.y += this.speedY;
-		if (this.x + this.speedX > myGameArea.canvas.width) {
+		
+		if (this.x > myGameArea.canvas.width) {
 			this.x = 0;
-		} else if (this.x + this.speedX < 0) {
+		}
+		if (this.x < 0) {
 			this.x = myGameArea.canvas.width;
-		} else if (this.y + this.speedY > myGameArea.canvas.height) {
+		}
+		if (this.y > myGameArea.canvas.height) {
 			this.y = 0;
-		} else if (this.y + this.speedY < 0) {
+		}
+		if (this.y < 0) {
 			this.y = myGameArea.canvas.height;
-		} 
+		}
 	}
-	this.collisionDetection = function(enemy) {
-		this.distance = Math.sqrt(Math.pow((this.x - enemy.x), 2) + Math.pow((this.y - enemy.y), 2));
-		if (this.distance < this.radius + enemy.radius) {
+	
+	this.collisionDetected = function(player) {
+		var dx = player.x - this.x;
+		var dy = player.y - this.y;
+		var distance = Math.sqrt(dx * dx + dy * dy);
+		
+		if (distance < this.radius + player.radius) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
 	this.startFollowing = function(player) {
-		this.distance = Math.sqrt(Math.pow((player.x - this.x), 2) + Math.pow((player.y - this.y), 2)); //расстояние между объектами
-		this.angle = Math.atan2(player.y - this.y, player.x - this.x) * 180 / Math.PI; //значение угла между объектами в градусах
-		this.amp = Math.sqrt(Math.pow(speedX, 2) + Math.pow(speedY, 2));
-		if (this.distance < (this.radius + player.radius) * 3) {
-			this.speedX = this.amp * Math.sin(this.angle);
-			this.speedY = this.amp * Math.cos(this.angle);
+		var dx = player.x - this.x;
+		var dy = player.y - this.y;
+		var distance = Math.sqrt(dx * dx + dy * dy); //расстояние между объектами
+		var angle = Math.atan2(dy, dx); //значение угла между объектами в радианах
+		var speed = Math.sqrt(this.speedX * this.speedX + this.speedY * this.speedY);
+		
+		if (distance < (this.radius + player.radius) * 3) {
+			this.speedX = speed * Math.cos(angle);
+			this.speedY = speed * Math.sin(angle);
 		} 
 	}
 }
@@ -78,32 +165,45 @@ function everyInterval(n) {
 	return false;
 }
 
-function randomNumber() { return (Math.random() < 0.5 ? -1 : 1)*(Math.random() * 5);}
+function randomNumber() { return (Math.random() < 0.5 ? -1 : 1)*(Math.random() * 3);}
 
 function randomPositionX() {return (Math.round(Math.random() * myGameArea.canvas.width));}
 
 function randomPositionY() {return (Math.round(Math.random() * myGameArea.canvas.height));}
 
-
-
 function moveGamePiece(event) {
 	myGamePiece.speedX = 0;
 	myGamePiece.speedY = 0;
+
 	if (event.keyCode == 37) {
-		myGamePiece.speedX = -1;
-	} else if (event.keyCode == 39) {
-		myGamePiece.speedX = 1;
-	} else if (event.keyCode == 38) {
-		myGamePiece.speedY = -1;
-	} else if (event.keyCode == 40) {
-		myGamePiece.speedY = 1;
+		myGamePiece.speedX = -4;
+	} 
+
+	if (event.keyCode == 39) {
+		myGamePiece.speedX = 4;
+	} 
+
+	if (event.keyCode == 38) {
+		myGamePiece.speedY = -4;
+	} 
+
+	if (event.keyCode == 40) {
+		myGamePiece.speedY = 4;
 	}
 }
 
 function updateGameArea() {
+	var ctx = myGameArea.context;
+
+	for (var j = 0; j < myOtherEnemies.length; j++) {
+		if(myGamePiece.collisionDetected(myOtherEnemies[j])) {
+			myGameArea.stop();
+			return;
+		} 
+	}
+	
 	for (var i = 0; i < myEnemies.length; i++) {
-		
-		if(myGamePiece.collisionDetection(myEnemies[i])) {
+		if(myGamePiece.collisionDetected(myEnemies[i])) {
 			myGameArea.stop();
 			return;
 		} 
@@ -112,17 +212,27 @@ function updateGameArea() {
 
 	myGameArea.frameNumber += 1;
 	if (myGameArea.frameNumber == 1 || everyInterval(150)) {
-		myEnemies.push(new component(randomPositionX(), randomPositionY(), 15, 'grey', randomNumber(), randomNumber()));
+		myEnemies.push(new component(randomPositionX(), randomPositionY(), 15, 'sprite-enemy.png', randomNumber(), randomNumber(), 'image'));
+		myOtherEnemies.push(new component(randomPositionX(), randomPositionY(), 15, 'sprite-another-enemy.png', randomNumber(), randomNumber(), 'image'));
 	}
-	for (i = 0; i < myEnemies.length; i++) {
-		myEnemies[i].startFollowing(myGamePiece);
-		myEnemies[i].newPos();
-		myEnemies[i].update();
-	}
-	myGamePiece.newPos();
-	myGamePiece.update();
-}
 
+	for (i = 0; i < myEnemies.length; i++) {
+		myEnemies[i].newPos();
+		myEnemies[i].draw(ctx);
+	}
+
+	for (j = 0; j < myOtherEnemies.length; j++) {
+		myOtherEnemies[j].startFollowing(myGamePiece);
+		myOtherEnemies[j].newPos();
+		myOtherEnemies[j].draw(ctx);
+	}
+
+	
+	myScore.text = "SCORE: " + Math.round(myGameArea.frameNumber / 30);
+	myScore.draw(ctx);
+	myGamePiece.newPos();
+	myGamePiece.draw(ctx);
+}
 
 startGame();
 
